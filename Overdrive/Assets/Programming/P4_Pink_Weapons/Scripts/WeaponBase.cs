4cf517ac2315b88e573121ad1ac16c92922997d0
Jacob Benitez <74565545+bogyjemm@users.noreply.github.com>
2026-02-17 01:12:45 -0500

    created WeaponBase

---
+++ Overdrive/Assets/Programming/P4_Pink_Weapons/Scripts/WeaponBase.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WeaponBase
+{
+    private string Name;
+    private bool IsMelee;
+    private float Range;
+    private float Damage;
+    private float Cooldown;
+    private float MagSize;
+    private float MaxCombo;
+}
